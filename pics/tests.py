a2eee40b3b08189b7f51d630010efc35986457fc
Annstella <annstellawangui@gmail.com>
2018-09-28 18:02:43 +0300

    test for image

--- pics/tests.py
+++ pics/tests.py
@@ -7,3 +7,13 @@ class ImageTestClass(TestCase):
     # Set up method
     def setUp(self):
         self.annstella= Image(name = 'house', description ='I like it', category ='basic', location = 'Nairobi')
+
+# Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.annstella,Image))
+
+ # Testing Save Method
+    def test_save_method(self):
+        self.annstella.save_image()
+        images = Images.objects.all()
+        self.assertTrue(len(images) > 0)
