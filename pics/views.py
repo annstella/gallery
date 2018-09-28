549e5666afc82bc95ce6a001b8971897c90b32a9
Annstella <annstellawangui@gmail.com>
2018-09-28 11:38:34 +0300

    removed convert date function

--- pics/views.py
+++ pics/views.py
@@ -5,6 +5,7 @@ import datetime as dt
 # Create your views here.
 def welcome(request):
     return render(request, 'welcome.html')
+    return render(request, 'all-pics/today-pics.html', {"date": date,})
 
 def pics_of_day(request):
     date = dt.date.today()
@@ -17,13 +18,13 @@ def pics_of_day(request):
             '''
     return HttpResponse(html)
 
-def convert_dates(dates):
+# def convert_dates(dates):
 
-    # Function that gets the weekday number for the date.
-    day_number = dt.date.weekday(dates)
+#     # Function that gets the weekday number for the date.
+#     day_number = dt.date.weekday(dates)
 
-    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
+#     days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
 
-    # Returning the actual day of the week
-    day = days[day_number]
-    return day
+#     # Returning the actual day of the week
+#     day = days[day_number]
+#     return day
