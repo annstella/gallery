a88d3c2206cea5c1248a330b75395ce4ab823de7
Annstella <annstellawangui@gmail.com>
2018-09-28 16:58:08 +0300

    created image test

--- pics/tests.py
+++ pics/tests.py
@@ -2,3 +2,8 @@ from django.test import TestCase
 from .models import Category,Location,Image
 
 # Create your tests here.
+class ImageTestClass(TestCase):
+
+    # Set up method
+    def setUp(self):
+        self.annstella= Image(name = 'house', description ='I like it', category ='basic', location = 'Nairobi')
