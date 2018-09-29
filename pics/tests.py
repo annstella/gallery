16665f6fa7b6329eb0a1745a39e551d8fe17f00c
Annstella <annstellawangui@gmail.com>
2018-09-29 11:02:02 +0300

    created a django admin

--- pics/tests.py
+++ pics/tests.py
@@ -1,19 +1,35 @@
 from django.test import TestCase
-from .models import Category,Location,Image
+from .models import Location,Category,Image
 
 # Create your tests here.
-class ImageTestClass(TestCase):
-
+class LocationTestClass(TestCase):
+  
     # Set up method
     def setUp(self):
-        self.annstella= Image(name = 'house', description ='I like it', category ='basic', location = 'Nairobi')
+        self.mombasa= Location(name = 'Mombasa', description ='Pwani ya Kenya')
 
-# Testing  instance
+    # Testing  instance
     def test_instance(self):
-        self.assertTrue(isinstance(self.annstella,Image))
+        self.assertTrue(isinstance(self.mombasa,Location))
 
- # Testing Save Method
+    # Testing Save Method
     def test_save_method(self):
-        self.annstella.save_image()
-        images = Images.objects.all()
-        self.assertTrue(len(images) > 0)
+        self.mombasa.save_location()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations) > 0)
+
+class CategoryTestClass(TestCase):
+  
+    # Set up method
+    def setUp(self):
+        self.health= Category(name = 'health', description ='An apple a day keeps the doctor away')
+
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.health,Category))
+
+    # Testing Save Method
+    def test_save_method(self):
+        self.health.save_category()
+        categories = Category.objects.all()
+        self.assertTrue(len(categories) > 0)
