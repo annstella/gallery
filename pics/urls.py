fe2281add9ec03e106f6393ed6486f850736598b
Annstella <annstellawangui@gmail.com>
2018-09-29 14:48:11 +0300

    configured urls.py to register the media root

--- pics/urls.py
+++ pics/urls.py
@@ -5,3 +5,5 @@ urlpatterns=[
    url('^$',views.pics,name = 'pics'),
     url(r'^search/', views.search_results, name='search_results')
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
